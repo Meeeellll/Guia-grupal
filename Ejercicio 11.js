613b345e1c7f0710cab9922730bf11ec1b708471
Melanie Elias <104339401+Meeeellll@users.noreply.github.com>
2022-12-05 23:02:21 -0600

    Primer commit

---
+++ Ejercicio 11.js
@@ -0,0 +1,13 @@
+/* Realizar programa donde se Introducen de 3 números y el no programa indica cuál es el
+mayor de los 3. */
+
+function numeromayor(){
+    var numero1 = 9
+    var numero2 = 14
+    var numero3 = 25
+
+    let mayor = Math.max(numero1, numero2, numero3)
+    console.log('El numero mayor es: '+mayor)
+}
+
+numeromayor()
